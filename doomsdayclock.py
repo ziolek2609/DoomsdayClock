503ed6f503001b1cb04da04496ac65cadcbc195b
ziolek2609 <ziolek2609@gmail.com>
2019-05-17 14:45:22 +0200

    Próba komunikacji danymi między stroną a pythonem + wklejony kod zegara; należy przeformatować na format datetime.datetim

--- doomsdayclock.py
+++ doomsdayclock.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 import datetime
 import time
 import os
@@ -7,8 +6,22 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def send():
+    if request.method =='POST':
+        eday = request.form['eday']
+        etime = request.form['etime']
+        end = datetime.datetime(eday,etime)
+        now = datetime.datetime.now()
+        delta = end - now
+        sec = round(delta.total_seconds())
+        years = sec//(365*24*60*60)
+        days = (sec%(365*24*60*60))//(24*60*60)
+        hours = (sec%(24*60*60))//(60*60)
+        mins = (sec%(60*60))//60
+        secs = (sec%60)
+        return render_template('zegar.html', years=years, days=days, hours=hours, mins=mins, secs=secs)
+
     return render_template('index.html')
 
 
