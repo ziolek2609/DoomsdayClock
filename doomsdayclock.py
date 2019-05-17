88f866e2c44fa1ee3c5bdfa4421268f3e09e7dc2
ziolek2609 <ziolek2609@gmail.com>
2019-05-17 15:33:25 +0200

    MAMY POKAZANE ILE CZASU ZOSTAŁO!

--- doomsdayclock.py
+++ doomsdayclock.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request
 import datetime
-import time
-import os
 
 app = Flask(__name__)
 
@@ -9,9 +7,14 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def send():
     if request.method =='POST':
-        eday = request.form['eday']
+        edate = request.form['edate']
+        date = edate.split("-")
         etime = request.form['etime']
-        end = datetime.datetime(eday,etime)
+        time = etime.split(":")
+        time.append(0)
+        print(date)
+        print(time)
+        end = datetime.datetime(int(date[0]),int(date[1]),int(date[2]),int(time[0]),int(time[1]),int(time[2]))
         now = datetime.datetime.now()
         delta = end - now
         sec = round(delta.total_seconds())
@@ -20,7 +23,7 @@ def send():
         hours = (sec%(24*60*60))//(60*60)
         mins = (sec%(60*60))//60
         secs = (sec%60)
-        return render_template('zegar.html', years=years, days=days, hours=hours, mins=mins, secs=secs)
+        return render_template('zegar.html', edate=edate, etime=etime, years=years, days=days, hours=hours, mins=mins, secs=secs)
 
     return render_template('index.html')
 
