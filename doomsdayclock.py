00eadb1509f420a1a388539ef34710f580d1b062
Johny198 <50331798+Johny198@users.noreply.github.com>
2019-05-23 14:43:33 +0200

    Update doomsdayclock.py

--- doomsdayclock.py
+++ doomsdayclock.py
@@ -1,60 +1,68 @@
-<html>
-  <head>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+# coding=utf-8
+from flask import Flask, render_template, request, jsonify
+import datetime
 
-      <title>DoomsdayClock</title>
-  </head>
-  <body>
-    <p style="text-align:center;font-size:25px;font-family:Verdana;color:#ffffff;background-color:#a6a6a6;">- - - - - DOOMSDAY WILL COME: {{ edate }} AT {{ etime }}- - - - -</p>
-    <div id="zegar" style="visibility:unset"></div>
-    <div style="visibility: hidden" align=center id="less_than_year">
-        <iframe src="https://giphy.com/embed/3orif3VHjBeYBDTGlG" width="280" height="162" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/season-16-the-simpsons-16x19-3orif3VHjBeYBDTGlG">via GIPHY</a></p>
-    </div>
-    <div style="visibility: hidden" align=center id="less_than_day">
-        <iframe src="https://giphy.com/embed/3orif3VHjBeYBDTGlG" width="280" height="162" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/season-16-the-simpsons-16x19-3orif3VHjBeYBDTGlG">via GIPHY</a></p>
-    </div>
-    <div style="visibility: hidden" align=center id="less_than_hour">
-        <iframe src="https://giphy.com/embed/KJ1f5iTl4Oo7u" width="380" height="170" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/just-thanks-KJ1f5iTl4Oo7u">via GIPHY</a></p>
-    </div>
-    <div style="visibility:hidden;text-align:center;font-size:25px;font-family:Verdana;color:#ffffff;background-color:#a6a6a6;"  id="the_end"></div>
-  <script>
-          window.setInterval(() => {
-            $.ajax({
-                url:'/timer',
-                data: {
-                    edate: '{{edate}}',
-                    etime: '{{etime}}'
-                },
-                type: 'POST',
-                success: function(response){
-                    console.log(response)
-                    document.getElementById("zegar").innerHTML = response.timer
-
-                    if(response.zegar)
-                      document.getElementById("zegar").style.visibility = "hidden"
-
-                    else
-                      document.getElementById("zegar").style.visibility = "unset"
-                    
-                    if (response.less_than_hour)
-                        document.getElementById("less_than_hour").style.visibility = "unset"
-                    else
-                        document.getElementById("less_than_hour").style.visibility = "hidden"
-                    if (response.less_than_day)
-                        document.getElementById("less_than_day").style.visibility = "unset"
-                    else
-                        document.getElementById("less_than_day").style.visibility = "hidden"
-                    if (response.less_than_year)
-                        document.getElementById("less_than_year").style.visibility = "unset"
-                    else
-                        document.getElementById("less_than_year").style.visibility = "hidden"
+app = Flask(__name__)
 
 
-                }
-            });
-            }, 1000);
-      </script>
-  </body>
-</html>
+@app.route('/', methods=['GET'])
+def send():
+    return render_template('index.html')
+
+
+@app.route('/timer', methods=['POST'])
+def timer():
+    edate = request.form['edate']  # pobieram wczytaną przez użytkownika datę
+    date = edate.split("-")  # format daty zamieniam na tablicę
+    etime = request.form['etime']  # pobieram wczytany przez użytkowanika czas
+    time = etime.split(":")  # format czasu zamieniam na tablicę
+    time.append(00)  # dodaję do tablicy czasu 00, ponieważ gdy użytkownik wpisze 00sekund format to pomija
+    # ustawiam datę końca świata za pomocą tablic daty i czasu
+    end = datetime.datetime(int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), int(time[2]))
+    now = datetime.datetime.now()
+    delta = end - now
+    sec = round(delta.total_seconds())
+    years = sec // (365 * 24 * 60 * 60)
+    days = (sec % (365 * 24 * 60 * 60)) // (24 * 60 * 60)
+    hours = (sec % (24 * 60 * 60)) // (60 * 60)
+    mins = (sec % (60 * 60)) // 60
+    secs = (sec % 60)
+    # różnica daty wpisanej i obecnej to pozostały czas do końca świata
+    # obliczenia w celu uzyskania wartości poszczególnych jednostek czasowych:
+    if delta.total_seconds()<0:
+        return jsonify(
+        timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs),
+        zegar = True
+
+        )
+    elif (3600-15)<delta.total_seconds() <= 3600:
+        return jsonify(
+            timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs),
+            less_than_hour=True
+            )
+
+    elif (60*60*24-15)<delta.total_seconds() <= (60*60*24):
+        return jsonify(
+            timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs),
+            less_than_day=True
+        )
+    elif (60*60*24*365-15)<delta.total_seconds()<=(60*60*365*24):
+        return jsonify(
+            timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs),
+            less_than_year=True
+            )
+    else:
+        return jsonify(
+        timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs)
+    )
+
+
+@app.route('/zegar', methods=['POST'])
+def czas():
+    edate = request.form['edate']
+    etime = request.form['etime']
+    return render_template('zegar.html', edate=edate, etime=etime)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
