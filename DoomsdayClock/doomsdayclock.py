0f2901addc353fec0f852e9c232b6d7b8d01e049
Johny198 <50331798+Johny198@users.noreply.github.com>
2019-05-24 17:47:10 +0200

    Update doomsdayclock.py

--- DoomsdayClock/doomsdayclock.py
+++ DoomsdayClock/doomsdayclock.py
@@ -1,5 +1,4 @@
-# coding=utf-8
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import datetime
 
 app = Flask(__name__)
@@ -14,28 +13,54 @@ def send():
 def timer():
     edate = request.form['edate']  # pobieram wczytaną przez użytkownika datę
     date = edate.split("-")  # format daty zamieniam na tablicę
-    etime = request.form['etime']  # pobieram wczytaną przez użytkowanika godzinę
-    time = etime.split(":")  # format godziny zamieniam na tablicę
-    time.append(00)  # dodaję do tablicy godziny 00, ponieważ gdy użytkownik wpisze 00sekund format to pomija
-    # ustawiam datę końca świata za pomocą tablic daty i godziny
+    etime = request.form['etime']  # pobieram wczytany przez użytkowanika czas
+    time = etime.split(":")  # format czasu zamieniam na tablicę
+    time.append(00)  # dodaję do tablicy czasu 00, ponieważ gdy użytkownik wpisze 00sekund format to pomija
+    # ustawiam datę końca świata za pomocą tablic daty i czasu
     end = datetime.datetime(int(date[0]), int(date[1]), int(date[2]), int(time[0]), int(time[1]), int(time[2]))
     now = datetime.datetime.now()
-    delta = end - now  # różnica daty wpisanej i obecnej to pozostały czas do końca świata
-    # obliczenia w celu uzyskania wartości poszczególnych jednostek czasowych:
+    delta = end - now
     sec = round(delta.total_seconds())
     years = sec // (365 * 24 * 60 * 60)
     days = (sec % (365 * 24 * 60 * 60)) // (24 * 60 * 60)
     hours = (sec % (24 * 60 * 60)) // (60 * 60)
     mins = (sec % (60 * 60)) // 60
     secs = (sec % 60)
-    # renderuje jednostki czasowe do timer.html w celu wyświetlenia odlicznika czasu
-    return render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs)
+    # różnica daty wpisanej i obecnej to pozostały czas do końca świata
+    # obliczenia w celu uzyskania wartości poszczególnych jednostek czasowych:
+    if delta.total_seconds()<0:
+        return jsonify(
+        timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs),
+        zegar = True,
+        the_end = True,
+        doomsday = True
+        )
+    elif (3600-20)<delta.total_seconds() <= 3600:
+        return jsonify(
+            timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs),
+            less_than_hour=True
+            )
+
+    elif (60*60*24-20)<delta.total_seconds() <= (60*60*24):
+        return jsonify(
+            timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs),
+            less_than_day=True
+        )
+    elif (60*60*24*365-20)<delta.total_seconds()<=(60*60*365*24):
+        return jsonify(
+            timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs),
+            less_than_year=True
+            )
+    else:
+        return jsonify(
+        timer=render_template('timer.html', years=years, days=days, hours=hours, mins=mins, secs=secs)
+    )
+
 
 @app.route('/zegar', methods=['POST'])
 def czas():
-    edate = request.form['edate'] # pobieram wczytaną przez użytkownika datę
-    etime = request.form['etime'] # pobieram wczytaną przez użytkowanika godzinę
-    # renderuje do zegar.html date i godzine pobraną od użytkownika, aby wyświtlić kiedy będzie koniec świata
+    edate = request.form['edate']
+    etime = request.form['etime']
     return render_template('zegar.html', edate=edate, etime=etime)
 
 
